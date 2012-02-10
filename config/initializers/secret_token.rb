1ad91a66006477f1acf09e1338b205cda6178ddd
root <root@surfdervish-ThinkPad-T400.(none)>
2012-02-09 22:25:21 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleScraper::Application.config.secret_token = 'c0773388c707f18da98d1ac2fe5ca8f29a530b43607a16c1bddd9b0b5cedff1a8295ae1831f0875ef81f742eb5e447abf0b3ad4fba0f966f71324c0fef628b08'
